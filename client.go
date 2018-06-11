5a85cbdb781422a36b9ea2a9ca4ad1bbdc046a23
Roman Budnikov <romanyx90@yandex.ru>
2018-06-11 16:08:03 +0300

    V2 client version

--- client.go
+++ client.go
@@ -50,9 +50,6 @@ const (
 )
 
 var (
-	// ErrResponse is returned when we were able to contact API but request was not successful and contains error details.
-	ErrResponse = errors.New("The MessageBird API returned an error")
-
 	// ErrUnexpectedResponse is used when there was an internal server error and nothing can be done at this point.
 	ErrUnexpectedResponse = errors.New("The MessageBird API is currently unavailable")
 )
@@ -63,118 +60,20 @@ type Client struct {
 	AccessKey  string       // The API access key
 	HTTPClient *http.Client // The HTTP client to send requests on
 	DebugLog   *log.Logger  // Optional logger for debugging purposes
-	request    func(c *Client, v interface{}, method, path string, data interface{}) error
-}
-
-// NewV2 creates a new MessageBird client object.
-// If the request to MessageBird API will return an
-// error, it will be returned as ErrorResponse instance
-// typed as the error, containing all codes,
-// descriptions and properies.
-func NewV2(accessKey string) *Client {
-	return &Client{
-		AccessKey: accessKey,
-		HTTPClient: &http.Client{
-			Timeout: httpClientTimeout,
-		},
-		request: requestV2,
-	}
 }
 
 // New creates a new MessageBird client object.
-// If the request to MessageBird API will return
-// and error, it will simply return ErrResponse.
-// Deprecated: use NewV2 until v2 release.
 func New(accessKey string) *Client {
 	return &Client{
 		AccessKey: accessKey,
 		HTTPClient: &http.Client{
 			Timeout: httpClientTimeout,
 		},
-		request: requestV1,
 	}
 }
 
 // Request is for internal use only and unstable.
 func (c *Client) Request(v interface{}, method, path string, data interface{}) error {
-	return c.request(c, v, method, path, data)
-}
-
-func requestV1(c *Client, v interface{}, method, path string, data interface{}) error {
-	if !strings.HasPrefix(path, "https://") && !strings.HasPrefix(path, "http://") {
-		path = fmt.Sprintf("%s/%s", Endpoint, path)
-	}
-	uri, err := url.Parse(path)
-	if err != nil {
-		return err
-	}
-
-	var jsonEncoded []byte
-	if data != nil {
-		jsonEncoded, err = json.Marshal(data)
-		if err != nil {
-			return err
-		}
-	}
-
-	request, err := http.NewRequest(method, uri.String(), bytes.NewBuffer(jsonEncoded))
-	if err != nil {
-		return err
-	}
-
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("Accept", "application/json")
-	request.Header.Set("Authorization", "AccessKey "+c.AccessKey)
-	request.Header.Set("User-Agent", "MessageBird/ApiClient/"+ClientVersion+" Go/"+runtime.Version())
-
-	if c.DebugLog != nil {
-		if data != nil {
-			c.DebugLog.Printf("HTTP REQUEST: %s %s %s", method, uri.String(), jsonEncoded)
-		} else {
-			c.DebugLog.Printf("HTTP REQUEST: %s %s", method, uri.String())
-		}
-	}
-
-	response, err := c.HTTPClient.Do(request)
-	if err != nil {
-		return err
-	}
-
-	defer response.Body.Close()
-
-	responseBody, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return err
-	}
-
-	if c.DebugLog != nil {
-		c.DebugLog.Printf("HTTP RESPONSE: %s", string(responseBody))
-	}
-
-	// Status code 500 is a server error and means nothing can be done at this
-	// point.
-	if response.StatusCode == 500 {
-		return ErrUnexpectedResponse
-	}
-	// Status codes 200 and 201 are indicative of being able to convert the
-	// response body to the struct that was specified.
-	if response.StatusCode == 200 || response.StatusCode == 201 {
-		if err := json.Unmarshal(responseBody, &v); err != nil {
-			return fmt.Errorf("could not decode response JSON, %s: %v", string(responseBody), err)
-		}
-		return nil
-	}
-
-	// We're dealing with an API error here. try to decode it, but don't do
-	// anything with the error. This is because not all values of `v` have
-	// `Error` properties and decoding could fail.
-	json.Unmarshal(responseBody, &v)
-
-	// Anything else than a 200/201/500 should be a JSON error.
-	return ErrResponse
-}
-
-func requestV2(c *Client, v interface{}, method, path string, data interface{}) error {
 	if !strings.HasPrefix(path, "https://") && !strings.HasPrefix(path, "http://") {
 		path = fmt.Sprintf("%s/%s", Endpoint, path)
 	}
@@ -253,10 +152,6 @@ func requestV2(c *Client, v interface{}, method, path string, data interface{}) 
 func (c *Client) Balance() (*Balance, error) {
 	balance := &Balance{}
 	if err := c.Request(balance, http.MethodGet, "balance", nil); err != nil {
-		if err == ErrResponse {
-			return balance, err
-		}
-
 		return nil, err
 	}
 
@@ -268,10 +163,6 @@ func (c *Client) Balance() (*Balance, error) {
 func (c *Client) HLR(id string) (*HLR, error) {
 	hlr := &HLR{}
 	if err := c.Request(hlr, http.MethodGet, HLRPath+"/"+id, nil); err != nil {
-		if err == ErrResponse {
-			return hlr, err
-		}
-
 		return nil, err
 	}
 
@@ -283,10 +174,6 @@ func (c *Client) HLR(id string) (*HLR, error) {
 func (c *Client) HLRs() (*HLRList, error) {
 	hlrList := &HLRList{}
 	if err := c.Request(hlrList, http.MethodGet, HLRPath, nil); err != nil {
-		if err == ErrResponse {
-			return hlrList, err
-		}
-
 		return nil, err
 	}
 
@@ -303,10 +190,6 @@ func (c *Client) NewHLR(msisdn string, reference string) (*HLR, error) {
 	hlr := &HLR{}
 
 	if err := c.Request(hlr, http.MethodPost, HLRPath, requestData); err != nil {
-		if err == ErrResponse {
-			return hlr, err
-		}
-
 		return nil, err
 	}
 
@@ -317,10 +200,6 @@ func (c *Client) NewHLR(msisdn string, reference string) (*HLR, error) {
 func (c *Client) Message(id string) (*Message, error) {
 	message := &Message{}
 	if err := c.Request(message, http.MethodGet, MessagePath+"/"+id, nil); err != nil {
-		if err == ErrResponse {
-			return message, err
-		}
-
 		return nil, err
 	}
 
@@ -336,10 +215,6 @@ func (c *Client) Messages(msgListParams *MessageListParams) (*MessageList, error
 	}
 
 	if err := c.Request(messageList, http.MethodGet, MessagePath+"?"+params.Encode(), nil); err != nil {
-		if err == ErrResponse {
-			return messageList, err
-		}
-
 		return nil, err
 	}
 
@@ -355,10 +230,6 @@ func (c *Client) NewMessage(originator string, recipients []string, body string,
 
 	message := &Message{}
 	if err := c.Request(message, http.MethodPost, MessagePath, requestData); err != nil {
-		if err == ErrResponse {
-			return message, err
-		}
-
 		return nil, err
 	}
 
@@ -369,10 +240,6 @@ func (c *Client) NewMessage(originator string, recipients []string, body string,
 func (c *Client) MMSMessage(id string) (*MMSMessage, error) {
 	mmsMessage := &MMSMessage{}
 	if err := c.Request(mmsMessage, http.MethodGet, MMSPath+"/"+id, nil); err != nil {
-		if err == ErrResponse {
-			return mmsMessage, err
-		}
-
 		return nil, err
 	}
 
@@ -391,10 +258,6 @@ func (c *Client) NewMMSMessage(originator string, recipients []string, msgParams
 
 	mmsMessage := &MMSMessage{}
 	if err := c.Request(mmsMessage, http.MethodPost, MMSPath, params); err != nil {
-		if err == ErrResponse {
-			return mmsMessage, err
-		}
-
 		return nil, err
 	}
 
@@ -405,10 +268,6 @@ func (c *Client) NewMMSMessage(originator string, recipients []string, msgParams
 func (c *Client) VoiceMessage(id string) (*VoiceMessage, error) {
 	message := &VoiceMessage{}
 	if err := c.Request(message, http.MethodGet, VoiceMessagePath+"/"+id, nil); err != nil {
-		if err == ErrResponse {
-			return message, err
-		}
-
 		return nil, err
 	}
 
@@ -419,10 +278,6 @@ func (c *Client) VoiceMessage(id string) (*VoiceMessage, error) {
 func (c *Client) VoiceMessages() (*VoiceMessageList, error) {
 	messageList := &VoiceMessageList{}
 	if err := c.Request(messageList, http.MethodGet, VoiceMessagePath, nil); err != nil {
-		if err == ErrResponse {
-			return messageList, err
-		}
-
 		return nil, err
 	}
 
@@ -438,10 +293,6 @@ func (c *Client) NewVoiceMessage(recipients []string, body string, params *Voice
 
 	message := &VoiceMessage{}
 	if err := c.Request(message, http.MethodPost, VoiceMessagePath, requestData); err != nil {
-		if err == ErrResponse {
-			return message, err
-		}
-
 		return nil, err
 	}
 
@@ -457,10 +308,6 @@ func (c *Client) NewVerify(recipient string, params *VerifyParams) (*Verify, err
 
 	verify := &Verify{}
 	if err := c.Request(verify, http.MethodPost, VerifyPath, requestData); err != nil {
-		if err == ErrResponse {
-			return verify, err
-		}
-
 		return nil, err
 	}
 
@@ -476,10 +323,6 @@ func (c *Client) VerifyToken(id, token string) (*Verify, error) {
 
 	verify := &Verify{}
 	if err := c.Request(verify, http.MethodGet, path, nil); err != nil {
-		if err == ErrResponse {
-			return verify, err
-		}
-
 		return nil, err
 	}
 
@@ -492,10 +335,7 @@ func (c *Client) Lookup(phoneNumber string, params *LookupParams) (*Lookup, erro
 	path := LookupPath + "/" + phoneNumber + "?" + urlParams.Encode()
 
 	lookup := &Lookup{}
-	if err := c.Request(lookup, http.MethodGet, path, nil); err != nil {
-		if err == ErrResponse {
-			return lookup, err
-		}
+	if err := c.Request(lookup, http.MethodPost, path, nil); err != nil {
 		return nil, err
 	}
 
@@ -509,10 +349,6 @@ func (c *Client) NewLookupHLR(phoneNumber string, params *LookupParams) (*HLR, e
 
 	hlr := &HLR{}
 	if err := c.Request(hlr, http.MethodPost, path, requestData); err != nil {
-		if err == ErrResponse {
-			return hlr, err
-		}
-
 		return nil, err
 	}
 
@@ -526,10 +362,6 @@ func (c *Client) LookupHLR(phoneNumber string, params *LookupParams) (*HLR, erro
 
 	hlr := &HLR{}
 	if err := c.Request(hlr, http.MethodGet, path, nil); err != nil {
-		if err == ErrResponse {
-			return hlr, err
-		}
-
 		return nil, err
 	}
 
