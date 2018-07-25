3fc804a9cec96b5aaf9bba4ac405a1eba9947539
Emile Pels <emile@messagebird.com>
2018-07-25 11:14:55 +0200

    Record last request to test server

--- internal/messagebirdtest/test_server.go
+++ internal/messagebirdtest/test_server.go
@@ -1,16 +1,29 @@
 package messagebirdtest
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"os"
 	"testing"
 )
 
+type resetFunc func()
+
+type request struct {
+	Body   []byte
+	Method string
+	URL    *url.URL
+}
+
+// Request contains the lastly received http.Request by the fake server.
+var Request request
+
 var server *httptest.Server
 
 var responseBody []byte
-var status int = 200
+var status int
 
 // EnableServer starts a fake server, runs the test and closes the server.
 func EnableServer(m *testing.M) {
@@ -23,6 +36,18 @@ func EnableServer(m *testing.M) {
 
 func initAndStartServer() {
 	server = httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		Request = request{
+			Method: r.Method,
+			URL:    r.URL,
+		}
+
+		var err error
+
+		Request.Body, err = ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err.Error())
+		}
+
 		// status and responseBody are defined in returns.go.
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(status)
@@ -36,20 +61,41 @@ func closeServer() {
 	server.Close()
 }
 
+// setRequest sets some basic fields from the http.Request in our global Request
+// struct.
+func setRequest(r *http.Request) error {
+	Request := request{
+		Method: r.Method,
+		URL:    r.URL,
+	}
+
+	var err error
+
+	// Reading from the request body is fine, as it's not used elsewhere.
+	// Server always returns fake data/testdata.
+	Request.Body, err = ioutil.ReadAll(r.Body)
+
+	return err
+}
+
+func WillReturn(b []byte, s int) {
+	responseBody = b
+	status = s
+}
+
 // WillReturnTestdata sets the status (s) for the test server to respond with.
 // Additionally it reads the bytes from the relativePath file and returns that
 // for requests. It fails the test if the file can not be read. The path is
 // relative to the testdata directory (the go tool ignores directories named
 // "testdata" in test packages: https://golang.org/cmd/go/#hdr-Test_packages).
 func WillReturnTestdata(t *testing.T, relativePath string, s int) {
-	responseBody = testdata(t, relativePath)
-	status = s
+	WillReturn(Testdata(t, relativePath), s)
 }
 
 // WillReturnAccessKeyError sets the response body and status for requests to
 // indicate the request is not allowed due to an incorrect access key.
 func WillReturnAccessKeyError() {
-	responseBody = []byte(`
+	WillReturn([]byte(`
 		{
 			"errors": [
 				{
@@ -58,6 +104,6 @@ func WillReturnAccessKeyError() {
 					"parameter":"access_key"
 				}
 			]
-		}`)
-	status = 401
+		}
+	`), 401)
 }
