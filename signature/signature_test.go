b38424b6f1c08cbec2416918169f2b3323cb78c7
Juanita De La Cuesta <juanitadelacuesta@MacBook-Pro-10.local>
2019-01-10 15:49:06 +0100

    Func: Add basic test for calculating signature

--- signature/signature_test.go
+++ signature/signature_test.go
@@ -1,1 +1,24 @@
 package signature
+
+import (
+	"bytes"
+	"encoding/base64"
+	"testing"
+)
+
+const testTs = "1544544948"
+const testQp = "abc=foo&def=bar"
+const testBody = `{"a key":"some value"}`
+const testSignature = "orb0adPhRCYND1WCAvPBr+qjm4STGtyvNDIDNBZ4Ir4="
+
+func TestCalculateSignature(t *testing.T) {
+	v := NewValidator("other-secret", 2, nil, nil)
+	s, err := v.CalculateSignature(testTs, testQp, []byte(testBody))
+	if err != nil {
+		t.Errorf("Error calculating signature: %s, expected: orb0adPhRCYND1WCAvPBr+qjm4STGtyvNDIDNBZ4Ir4=", s)
+	}
+	drs, _ := base64.StdEncoding.DecodeString(testSignature)
+	if bytes.Compare(s, drs) != 0 {
+		t.Errorf("Unexpected signature: %s, expected: orb0adPhRCYND1WCAvPBr+qjm4STGtyvNDIDNBZ4Ir4=", s)
+	}
+}
