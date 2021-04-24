5f2711ddd04b6a4c8035a7d1b8f4d90371e020b7
Aleksey Salnikov <me@iamsalnikov.ru>
2021-04-24 17:24:10 +0300

    Test errors
    I've written tests for error.go file that covers error messages format

--- error_test.go
+++ error_test.go
@@ -1,8 +1,9 @@
 package messagebird
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestError(t *testing.T) {
@@ -36,4 +37,62 @@ func TestError(t *testing.T) {
 		}
 		assert.Error(t, errRes)
 	})
+}
+
+func TestError_ErrorMessageContainsOnlyDescription(t *testing.T) {
+	err := Error{
+		Code:        42,
+		Description: "something bad",
+		Parameter:   "foo",
+	}
+
+	assert.Equal(t, "something bad", err.Error())
+}
+
+func TestErrorResponse_ErrorMessageFormat(t *testing.T) {
+	err1 := Error{
+		Code:        42,
+		Description: "something bad",
+		Parameter:   "foo",
+	}
+
+	err2 := Error{
+		Code:        43,
+		Description: "something else",
+		Parameter:   "bar",
+	}
+
+	cases := map[string]struct {
+		errors []Error
+		exp    string
+	}{
+		"nil errors slice": {
+			errors: nil,
+			exp:    "API errors: ",
+		},
+		"one error": {
+			errors: []Error{
+				err1,
+			},
+			exp: "API errors: " + err1.Error(),
+		},
+		"two errors separated with coma": {
+			errors: []Error{
+				err1,
+				err2,
+			},
+			exp: "API errors: " + err1.Error() + ", " + err2.Error(),
+		},
+	}
+
+	for n, c := range cases {
+		t.Run(n, func(t *testing.T) {
+			err := ErrorResponse{
+				Errors: c.errors,
+			}
+
+			assert.Equal(t, c.exp, err.Error())
+		})
+	}
+
 }
